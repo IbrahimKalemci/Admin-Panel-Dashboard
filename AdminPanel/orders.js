d39ed25e4d0d7182c3122bc041624da696340987
IbrahimKalemci <githubibo@gmail.com>
2023-06-12 12:23:45 +0300

    Admin panel DashBoard
    admin panel full responsive ve karanlık temalı

---
+++ AdminPanel/orders.js
@@ -0,0 +1,51 @@
+const Orders =[
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Larvender KF102 Drone',
+        productNumber:'36378',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName:'Asus RogStrix RTX 4080',
+        productNumber:'56408',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Msi SuprimX RTX 4070 TI',
+        productNumber:'98325',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName:'Loctan octain RX7900XT OC',
+        productNumber:'34790',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName:'Felicon RK2 Keyboard Red SW',
+        productNumber:'43252',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName:'Asus ZenBook ZL-53 Special',
+        productNumber:'73346',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName:'DJI Air 25 Rapid Drone',
+        productNumber:'04996',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    }
+   
+]
